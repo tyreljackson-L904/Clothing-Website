2376bf4fb655141e069c34a3c699934266cd2fd9
Tyrel Jackson <tyreljackson904@gmail.com>
2022-02-21 14:09:12 -0700

    homepage componenets/styling

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {HomePage} from "./components/index"
 
 function App() {
   return (
     <div>
-     
+     <HomePage />
     </div>
   );
 }
---
+++ crwn-clothing/src/components/index.js
@@ -0,0 +1,3 @@
+export {default as HomePage} from "../pages/homepage/HomePage"
+export { default as MenuItem } from "./menu-item/MenuItem"
+export { default as Directory } from "./directory/Directory"
